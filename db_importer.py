d7e2163b8d5c0c6e2279cda80e97602ea4372337
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2022-04-12 19:55:32 -0300

    Add black formatter & pre-commit

--- db_importer.py
+++ db_importer.py
@@ -2,9 +2,22 @@
 
 import click
 
-@click.cli
-def run():
+
+@click.command()
+@click.option(
+    "-i",
+    "--input",
+    type=click.Path(file_okay=True, exists=True, dir_okay=False),
+)
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(file_okay=False, exists=False, dir_okay=True),
+)
+def run(input: str, output: str):
     """"""
+    print(input)
+    print(output)
 
 
 if __name__ == "__main__":
