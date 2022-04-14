2e350b9db8162cb9f2ca99a831fb72091edeaf8f
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2022-04-13 23:23:14 -0300

    Add constructor & driver schemas

--- importer/schema.py
+++ importer/schema.py
@@ -1,12 +1,12 @@
 """SQL Statements for importing CSV data."""
 
 import csv
-from sqlite3 import Connection
+from sqlite3 import Connection, ProgrammingError
 
 import click
 
 CIRCUITS_CREATE = """CREATE TABLE circuits (
-    circuitId INTEGER PRIMARY KEY NOT NULL,
+    circuitId INTEGER PRIMARY KEY,
     circuitRef TEXT NOT NULL,
     name TEXT NOT NULL,
     location TEXT,
@@ -32,7 +32,7 @@ CIRCUITS_INSERT = """INSERT INTO circuits VALUES (
 """
 
 CONSTRUCTOR_RESULTS_CREATE = """CREATE TABLE constructor_results (
-    constructorResultsId INTEGER PRIMARY KEY NOT NULL,
+    constructorResultsId INTEGER PRIMARY KEY,
     raceId INTEGER NOT NULL ,
     constructorId INTEGER NOT NULL ,
     points REAL,
@@ -49,16 +49,108 @@ CONSTRUCTOR_RESULTS_INSERT = """INSERT INTO constructor_results VALUES (
 )
 """
 
+CONSTRUCTOR_STANDINGS_CREATE = """CREATE TABLE constructor_standings (
+    constructorStandingsId INTEGER PRIMARY KEY,
+    raceId INTEGER NOT NULL,
+    constructorId INTEGER NOT NULL,
+    points REAL NOT NULL,
+    position INTEGER,
+    positionText TEXT,
+    wins INTEGER NOT NULL
+)
+"""
+
+CONSTRUCTOR_STANDINGS_INSERT = """INSERT INTO constructor_standings VALUES (
+    :constructorStandingsId,
+    :raceId,
+    :constructorId,
+    :points,
+    :position,
+    :positionText,
+    :wins
+)
+"""
+
+CONSTRUCTORS_CREATE = """CREATE TABLE constructors (
+    constructorId INTEGER PRIMARY KEY,
+    constructorRef TEXT NOT NULL,
+    name TEXT NOT NULL UNIQUE,
+    nationality TEXT,
+    url TEXT NOT NULL
+)
+"""
+
+CONSTRUCTORS_INSERT = """INSERT INTO constructors VALUES (
+    :constructorId,
+    :constructorRef,
+    :name,
+    :nationality,
+    :url
+)
+"""
+
+DRIVER_STANDINGS_CREATE = """CREATE TABLE driver_standings (
+    driverStandingsId INTEGER PRIMARY KEY,
+    raceId INTEGER NOT NULL,
+    driverId INTEGER NOT NULL,
+    points REAL NOT NULL,
+    position INTEGER,
+    positionText TEXT,
+    wins INTEGER
+)
+"""
+
+DRIVER_STANDINGS_INSERT = """INSERT INTO driver_standings VALUES (
+    :driverStandingsId,
+    :raceId,
+    :driverId,
+    :points,
+    :position,
+    :positionText,
+    :wins
+)
+"""
+
+DRIVERS_CREATE = """CREATE TABLE drivers (
+    driverId INTEGER PRIMARY KEY,
+    driverRef TEXT NOT NULL,
+    number INTEGER,
+    code TEXT,
+    forename TEXT NOT NULL,
+    surname TEXT NOT NULL,
+    dob TEXT,
+    nationality TEXT,
+    url TEXT NOT NULL
+)
+"""
+
+DRIVERS_INSERT = """INSERT INTO drivers VALUES (
+    :driverId,
+    :driverRef,
+    :number,
+    :code,
+    :forename,
+    :surname,
+    :dob,
+    :nationality,
+    :url
+)
+"""
+
+
 TABLE_MAP = {
     "circuits": (CIRCUITS_CREATE, CIRCUITS_INSERT),
     "constructor_results": (
         CONSTRUCTOR_RESULTS_CREATE,
         CONSTRUCTOR_RESULTS_INSERT,
     ),
-    # "constructor_standings",
-    # "constructors",
-    # "driver_standings",
-    # "drivers",
+    "constructor_standings": (
+        CONSTRUCTOR_STANDINGS_CREATE,
+        CONSTRUCTOR_STANDINGS_INSERT,
+    ),
+    "constructors": (CONSTRUCTORS_CREATE, CONSTRUCTORS_INSERT),
+    "driver_standings": (DRIVER_STANDINGS_CREATE, DRIVER_STANDINGS_INSERT),
+    "drivers": (DRIVERS_CREATE, DRIVERS_INSERT),
     # "lap_times",
     # "pit_stops",
     # "qualifying",
@@ -82,6 +174,11 @@ def import_table(con: Connection, name: str, content: csv.DictReader) -> None:
     cur.execute(f"DROP TABLE IF EXISTS {name}")
     cur.execute(create_sql)
     for row in content:
-        cur.execute(insert_sql, row)
+        # TODO: Null values are comming as "\\N"
+        try:
+            cur.execute(insert_sql, row)
+        except ProgrammingError as exc:
+            click.secho(f"{exc}:\nRow Content: {row}")
+            return
 
     con.commit()
