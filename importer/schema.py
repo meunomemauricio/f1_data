e0fe14752e99b6b78cdef7ac5df454f2d8b8f58e
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2022-04-14 15:21:52 -0300

    Finish importer schema

--- importer/schema.py
+++ importer/schema.py
@@ -137,6 +137,206 @@ DRIVERS_INSERT = """INSERT INTO drivers VALUES (
 )
 """
 
+LAP_TIMES_CREATE = """CREATE TABLE lap_times (
+    raceId INTEGER,
+    driverId INTEGER,
+    lap INTEGER,
+    position INTEGER,
+    time TEXT,
+    milliseconds INTEGER
+)
+"""
+
+LAP_TIMES_INSERT = """INSERT INTO lap_times VALUES (
+    :raceId,
+    :driverId,
+    :lap,
+    :position,
+    :time,
+    :milliseconds
+)
+"""
+
+PIT_STOPS_CREATE = """CREATE TABLE pit_stops (
+    raceId INTEGER,
+    driverId INTEGER,
+    stop INTEGER,
+    lap INTEGER NOT NULL,
+    time TEXT NOT NULL,
+    duration TEXT,
+    milliseconds INTEGER
+)
+"""
+
+PIT_STOPS_INSERT = """INSERT INTO pit_stops VALUES (
+    :raceId,
+    :driverId,
+    :stop,
+    :lap,
+    :time,
+    :duration,
+    :milliseconds
+)
+"""
+
+QUALIFYING_CREATE = """CREATE TABLE qualifying (
+    qualifyId INTEGER PRIMARY KEY,
+    raceId INTEGER NOT NULL,
+    driverId INTEGER NOT NULL,
+    constructorId INTEGER NOT NULL,
+    number INTEGER NOT NULL,
+    position INTEGER,
+    q1 TEXT,
+    q2 TEXT,
+    q3 TEXT
+)
+"""
+
+QUALIFYING_INSERT = """INSERT INTO qualifying VALUES (
+    :qualifyId,
+    :raceId,
+    :driverId,
+    :constructorId,
+    :number,
+    :position,
+    :q1,
+    :q2,
+    :q3
+)
+"""
+
+# NOTE: Ignoring fp1/fp2/fp3/quali/sprint dates & times fields
+RACES_CREATE = """CREATE TABLE races (
+    raceId INTEGER PRIMARY KEY,
+    year INTEGER NOT NULL,
+    round INTEGER NOT NULL,
+    circuitId INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    date TEXT NOT NULL,
+    time TEXT,
+    url TEXT UNIQUE
+)
+"""
+
+RACES_INSERT = """INSERT INTO races VALUES (
+    :raceId,
+    :year,
+    :round,
+    :circuitId,
+    :name,
+    :date,
+    :time,
+    :url
+)
+"""
+
+RESULTS_CREATE = """CREATE TABLE results (
+    resultId INTEGER PRIMARY KEY,
+    raceId INTEGER NOT NULL,
+    driverId INTEGER NOT NULL,
+    constructorId INTEGER NOT NULL,
+    number INTEGER,
+    grid INTEGER NOT NULL,
+    position INTEGER,
+    positionText TEXT NOT NULL,
+    positionOrder INTEGER NOT NULL,
+    points REAL NOT NULL,
+    laps INTEGER NOT NULL,
+    time TEXT,
+    milliseconds INTEGER,
+    fastestLap INTEGER,
+    rank INTEGER,
+    fastestLapTime TEXT,
+    fastestLapSpeed TEXT,
+    statusId INTEGER NOT NULL
+)"""
+
+RESULTS_INSERT = """INSERT INTO results VALUES (
+    :resultId,
+    :raceId,
+    :driverId,
+    :constructorId,
+    :number,
+    :grid,
+    :position,
+    :positionText,
+    :positionOrder,
+    :points,
+    :laps,
+    :time,
+    :milliseconds,
+    :fastestLap,
+    :rank,
+    :fastestLapTime,
+    :fastestLapSpeed,
+    :statusId
+)
+"""
+
+SEASONS_CREATE = """CREATE TABLE seasons (
+    year INTEGER PRIMARY KEY,
+    url TEXT NOT NULL
+)
+"""
+
+SEASONS_INSERT = """INSERT INTO seasons VALUES (
+    :year,
+    :url
+)
+"""
+
+SPRINT_RESULTS_CREATE = """CREATE TABLE sprint_results (
+    resultId INTEGER PRIMARY KEY,
+    raceId INTEGER NOT NULL,
+    driverId INTEGER NOT NULL,
+    constructorId INTEGER NOT NULL,
+    number INTEGER,
+    grid INTEGER NOT NULL,
+    position INTEGER,
+    positionText TEXT NOT NULL,
+    positionOrder INTEGER NOT NULL,
+    points REAL NOT NULL,
+    laps INTEGER NOT NULL,
+    time TEXT,
+    milliseconds INTEGER,
+    fastestLap INTEGER,
+    fastestLapTime TEXT,
+    statusId INTEGER NOT NULL
+)
+"""
+
+SPRINT_RESULTS_INSERT = """INSERT INTO sprint_results VALUES (
+    :resultId,
+    :raceId,
+    :driverId,
+    :constructorId,
+    :number,
+    :grid,
+    :position,
+    :positionText,
+    :positionOrder,
+    :points,
+    :laps,
+    :time,
+    :milliseconds,
+    :fastestLap,
+    :fastestLapTime,
+    :statusId
+)
+"""
+
+STATUS_CREATE = """CREATE TABLE status (
+    statusId INTEGER PRIMARY KEY,
+    status TEXT
+)
+"""
+
+STATUS_INSERT = """INSERT INTO status VALUES (
+    :statusId,
+    :status
+)
+"""
+
 
 TABLE_MAP = {
     "circuits": (CIRCUITS_CREATE, CIRCUITS_INSERT),
@@ -151,14 +351,14 @@ TABLE_MAP = {
     "constructors": (CONSTRUCTORS_CREATE, CONSTRUCTORS_INSERT),
     "driver_standings": (DRIVER_STANDINGS_CREATE, DRIVER_STANDINGS_INSERT),
     "drivers": (DRIVERS_CREATE, DRIVERS_INSERT),
-    # "lap_times",
-    # "pit_stops",
-    # "qualifying",
-    # "races",
-    # "results",
-    # "seasons",
-    # "sprint_results",
-    # "status",
+    "lap_times": (LAP_TIMES_CREATE, LAP_TIMES_INSERT),
+    "pit_stops": (PIT_STOPS_CREATE, PIT_STOPS_INSERT),
+    "qualifying": (QUALIFYING_CREATE, QUALIFYING_INSERT),
+    "races": (RACES_CREATE, RACES_INSERT),
+    "results": (RESULTS_CREATE, RESULTS_INSERT),
+    "seasons": (SEASONS_CREATE, SEASONS_INSERT),
+    "sprint_results": (SPRINT_RESULTS_CREATE, SPRINT_RESULTS_INSERT),
+    "status": (STATUS_CREATE, STATUS_INSERT),
 }
 
 
@@ -174,11 +374,11 @@ def import_table(con: Connection, name: str, content: csv.DictReader) -> None:
     cur.execute(f"DROP TABLE IF EXISTS {name}")
     cur.execute(create_sql)
     for row in content:
-        # TODO: Null values are comming as "\\N"
+        # TODO: Null values are coming as "\\N"
         try:
             cur.execute(insert_sql, row)
         except ProgrammingError as exc:
-            click.secho(f"{exc}:\nRow Content: {row}")
+            click.secho(f"{exc}:\nRow Content: {row}", fg="bright_red")
             return
 
     con.commit()
